80c436717a5249254e66258215e03b9be5b532a3
Jim Gumbley <jgumbley@thoughtworks.com>
2019-03-08 12:47:44 +0000

    init firebase app and made it say hello world

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+admin.initializeApp(functions.config().firebase);
+const app = express();
+const main = express();
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+// webApi is your functions name, and you will pass main as 
+export const webApi = functions.https.onRequest(main);
+
+// wiring things up
+app.post('/helloworld', (req, res) => {
+    res.send('Yo blair');
+})
